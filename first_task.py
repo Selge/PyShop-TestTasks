29e4b551611d38434098b86ee916fc698a9be48e
Sergei Shcherbak <sshch1987@gmail.com>
2022-11-11 20:18:10 +0200

    First

---
+++ first_task.py
@@ -0,0 +1,84 @@
+# В примере кода ниже генерируется список фиксаций состояния счета игры в течение матча.
+# Разработайте функцию get_score(game_stamps, offset), которая вернет счет на момент offset в списке game_stamps.
+# Нужно суметь понять суть написанного кода, заметить нюансы,
+# разработать функцию вписывающуюся стилем в существующий код, желательно адекватной алгоритмической сложности.
+
+
+from pprint import pprint
+import random
+import math
+
+TIMESTAMPS_COUNT = 50000
+
+PROBABILITY_SCORE_CHANGED = 0.0001
+
+PROBABILITY_HOME_SCORE = 0.45
+
+OFFSET_MAX_STEP = 3
+
+INITIAL_STAMP = {
+    "offset": 0,
+    "score": {
+        "home": 0,
+        "away": 0
+    }
+}
+
+
+def generate_stamp(previous_value):
+    score_changed = random.random() > 1 - PROBABILITY_SCORE_CHANGED
+    home_score_change = 1 if score_changed and random.random() > 1 - PROBABILITY_HOME_SCORE else 0
+    away_score_change = 1 if score_changed and not home_score_change else 0
+    offset_change = math.floor(random.random() * OFFSET_MAX_STEP) + 1
+
+    return {
+        "offset": previous_value["offset"] + offset_change,
+        "score": {
+            "home": previous_value["score"]["home"] + home_score_change,
+            "away": previous_value["score"]["away"] + away_score_change
+        }
+    }
+
+
+def generate_game():
+    stamps = [INITIAL_STAMP, ]
+    current_stamp = INITIAL_STAMP
+    for _ in range(TIMESTAMPS_COUNT):
+        current_stamp = generate_stamp(current_stamp)
+        stamps.append(current_stamp)
+
+    return stamps
+
+
+game_stamps = generate_game()
+
+# pprint(game_stamps)
+
+
+def get_score(game_stamps, offset=None):
+    """
+        Takes list of game's stamps and time offset for which returns the scores for the home and away teams.
+        Please pay attention to that for some offsets the game_stamps list may not contain scores.
+    """
+    # offset = int(input())
+    err_message = "No data!"
+    offsets = []
+    for game_stamp in game_stamps:
+        offsets.append(game_stamp["offset"])
+
+    if offset not in offsets:
+        return err_message
+    else:
+        for game_stamp in game_stamps:
+            stamp = list(game_stamp.values())
+            for check in stamp:
+                if check == offset:
+                    score = stamp[1]
+                    home = score.get('home') if score else 0
+                    away = score.get('away') if score else 0
+                    # return home, away
+                    return home, away
+
+
+if __name__ == '__main__':
+    get_score(game_stamps)
---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+from first_task import *
+# Для разработанной в предыдущем задании функции get_score(game_stamps, offset)
+# разработайте unit-тесты на фреймворке unittest.
+# Тесты должны учитывать все возможные случаи использования функции,
+# концентрироваться на проверке одного случая, не повторяться,
+# название тестов должно отражать суть выполняемой проверки.
+
+
+class TestMethods(unittest.TestCase):
+    def first_test(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
